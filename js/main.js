92fd32b78d1f0277bc04f9b10a9c59d1b4cb0cc4
afranek <128502600+afranek@users.noreply.github.com>
2023-03-26 15:20:59 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let JS = "Uwielbiam JavaScript";
+let Pro = "Jestem świetnym programistą";
+
+function stringLength(x,y) {
+    if (x.length > y.length) {
+        console.log(x);
+        } else{
+        console.log(y);
+    }
+}
+stringLength(JS,Pro);
